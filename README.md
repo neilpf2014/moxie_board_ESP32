17800431b920c004b6109a0bfc76c8dbd7bd0126
neilpf2014 <neilpf2014@gmail.com>
2019-10-05 02:57:00 -0400

    added QoS workaround

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# Moxie board code
+# Moxie board code for ESP32
 
 For new PRS Moxieboard controler using MQTT to send teams button presses back via Wifi
-This uses the ESP32
+modifed to use only the MCU pins now.
+Will send a string starting with UID followed by comma separated list of button presses since the 
+last successful send operation.
+Success is confimed by sending back the UID to the MCU.
 
  #*This is a WIP*
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,9 @@
 NF 2019-09-24
 
 Mashup of code from ESP8266 MQTT example --  rewritten from pub sub example to use the wrapper class
-MQTThandler and Shift register button handling routine.  Since this is running on ESP32, will need to use
+MQTThandler.  Since this is running on ESP32, will need to use
 Stage version of ESP32 core and Dev version of WiFiManager (See PIO ini file)
+This now reads 20 button using 9 GPIO's
 This will become the new Fubar Moxie board control code
 Will collect button presses into array and then send them over to server via MQTT on intervals.
 * ****************************************************************************************************
@@ -15,8 +16,9 @@ Will collect button presses into array and then send them over to server via MQT
 #include <WebServer.h>
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
-#define NUM_BTNS 20 // number of button to be read
-// all of the ESP32 pins that will be used
+#define NUM_BTNS 20 // number of buttons to be read
+// all of the ESP32 button GPIO's 
+// This needs to match the carrier PCB
 #define OUTPIN_1 23
 #define OUTPIN_2 22
 #define OUTPIN_3 21
@@ -27,13 +29,12 @@ Will collect button presses into array and then send them over to server via MQT
 #define INPIN_3 32
 #define INPIN_4 33
 
-
 uint8_t OutPins[5] = {OUTPIN_1, OUTPIN_2, OUTPIN_3, OUTPIN_4, OUTPIN_5};
 uint8_t InPins[4] = {INPIN_1, INPIN_2, INPIN_3, INPIN_4};
+// used to deal with button presses
 uint32_t BtnTemp[NUM_BTNS];
 uint32_t BtnRecord[NUM_BTNS];
 uint32_t BtnsOutgoing[NUM_BTNS];
-
 uint32_t CurrOutPin = 0;
 uint32_t CurrInPin = 0;
 
@@ -81,11 +82,16 @@ IPAddress MQTTIp(192,168,1,117); // IP oF the MQTT broker
 
 WiFiClient espClient;
 uint64_t lastMsg = 0;
+uint64_t MessID;
+
+uint64_t msgPeriod = 5000; //Message check interval in ms
+uint32_t MsgSendNewPer;
 String S_msg;
 String BtnArraySend; // hold CSV of button array
 int value = 0;
 uint8_t GotMail;
 uint8_t statusCode;
+uint8_t BtnMessSuccess;
 uint8_t ConnectedToAP = false;
 //MQTThandler MTQ(espClient, svrName);
 MQTThandler MTQ(espClient, MQTTIp);
@@ -123,22 +129,36 @@ void WiFiCP(uint8_t ResetAP)
 	MTQ.subscribeIncomming("ConfirmMsg");
 	MTQ.subscribeOutgoing("BtnsOut");
 }
-void setup() {
-	pinMode(LED_BUILTIN, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
-	Serial.begin(115200);
-	WiFiCP(true);
-	//WiFiManager wifiManager;
-	//wifiManager.setAPCallback(configModeCallback);
-	//wifiManager.autoConnect("AutoConnectAP");
-	//Serial.println("Print IP:");
-	//Serial.println(WiFi.localIP());
-	
-	for (size_t i = 0; i < 5; i++)
-		pinMode(OutPins[i], OUTPUT);
-	for (size_t i = 0; i < 4; i++)
-		pinMode(InPins[i], INPUT_PULLDOWN);
-	Serial.println("Started");
+
+// send CSV line via MQTT
+void SendNewBtnMessage(){
+	MessID = millis();
+	memcpy(BtnRecord, BtnsOutgoing, NUM_BTNS);
+		BtnArraySend = "M" + MessID;
+		for (size_t i = 0; i < NUM_BTNS; i++){
+			BtnArraySend = BtnArraySend + "," + String(BtnsOutgoing[i]);
+		}
+		statusCode = MTQ.publish(BtnArraySend);
+		// reset the button counter
+		for (size_t i = 0; i < NUM_BTNS; i++)
+			BtnRecord[i] = 0;
+		BtnMessSuccess = false;
 }
+
+uint8_t MessageTest(String test)
+{
+   String tempST;
+   uint64_t MessTest = 0;
+   if(S_msg.startsWith("M")){
+      tempST = test.substring(2);
+      MessTest = tempST.toInt();
+	}
+   if (MessTest == MessID)
+      return true;
+   else
+      return false;
+}
+
 // For toggle of led on/off
 void LedCheck(char Ckc){
 	if (Ckc == '1') {
@@ -150,6 +170,25 @@ void LedCheck(char Ckc){
 		digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
 	}
 }
+
+void setup() {
+	pinMode(LED_BUILTIN, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+	Serial.begin(115200);
+	WiFiCP(true);
+	//WiFiManager wifiManager;
+	//wifiManager.setAPCallback(configModeCallback);
+	//wifiManager.autoConnect("AutoConnectAP");
+	//Serial.println("Print IP:");
+	//Serial.println(WiFi.localIP());
+	BtnMessSuccess = false;
+   MsgSendNewPer = 0;
+	for (size_t i = 0; i < 5; i++)
+		pinMode(OutPins[i], OUTPUT);
+	for (size_t i = 0; i < 4; i++)
+		pinMode(InPins[i], INPUT_PULLDOWN);
+	Serial.println("Started");
+}
+
 // main loop
 void loop() {
 	now = millis();
@@ -173,34 +212,38 @@ void loop() {
          }
 	   }
    }
+   // check status here, still test code needs work !!!!
 	GotMail = MTQ.update();
-	// check status here, still test code needs work !!!!
 	if (GotMail == true){
+
+		//**test code to be removed ****************
 		Serial.print("message is: ");
 		S_msg = MTQ.GetMsg();
 		Serial.println(S_msg);
 		LedCheck(S_msg.charAt(0));
+		// ******************************************
+		BtnMessSuccess = MessageTest(S_msg);
 		GotMail = false;
 	}
-	
-	// push out a message every 1 min for testing remove
-	// Todo make sure to code a check status before send !!!!!
-	if (now - lastMsg > 60000) {
+	// check every n sec to see if last message rc'ved
+	// if so push out a new message every 2 min (n * 20)
+   // remove for testing 
+	if (now - lastMsg > msgPeriod) {
 		lastMsg = now;
+      MsgSendNewPer++;
+      if((BtnArraySend != "") && (!BtnMessSuccess))
+         statusCode = MTQ.publish(BtnArraySend);
+      if(BtnMessSuccess && (MsgSendNewPer > 20)){
+         SendNewBtnMessage();
+         MsgSendNewPer = 0;
+      }
+		//**test code to be removed ****************
 		++value;
 		S_msg = "string message # " + String(value);
 		Serial.print("Publish message (main): ");
 		Serial.println(S_msg);
 		statusCode = MTQ.publish(S_msg);
-		// send CSV line
-		memcpy(BtnRecord, BtnsOutgoing, NUM_BTNS);
-		BtnArraySend = "A";
-		for (size_t i = 0; i < NUM_BTNS; i++){
-			BtnArraySend = BtnArraySend + "," + String(BtnsOutgoing[i]);
-		}
-		statusCode = MTQ.publish(BtnArraySend);
-		// reset the button counter
-		for (size_t i = 0; i < NUM_BTNS; i++)
-			BtnRecord[i] = 0;
+		// ******************************************
+		
 	}
 }
