382f453fb196738ae38bb2ae6718c320cb5f3387
neilpf2014 <neilpf2014@gmail.com>
2019-10-20 00:25:40 -0400

    fix ESP32 crash issue, clean up code

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ Simplifed version with no QOS check on if the message was recv'ed
 #define INPIN_3 32
 #define INPIN_4 33
 
-#define BTN_DELAY 500 // lockout delay in ms
+#define BTN_DELAY 2500 // lockout delay in ms
 
 uint8_t OutPins[5] = {OUTPIN_1, OUTPIN_2, OUTPIN_3, OUTPIN_4, OUTPIN_5};
 uint8_t InPins[4] = {INPIN_1, INPIN_2, INPIN_3, INPIN_4};
@@ -47,9 +47,6 @@ uint32_t BtnRecord[NUM_BTNS];
 
 uint32_t CurrOutPin = 0;
 uint32_t CurrInPin = 0;
-
-uint32_t PressedBtnIndex = 0;
-uint32_t LastPressIndex = 0;
 
 uint64_t now;
 uint64_t PasTime = 0;
@@ -91,10 +88,10 @@ IPAddress MQTTIp(192,168,1,117); // IP oF the MQTT broker
 
 WiFiClient espClient;
 uint64_t lastMsg = 0;
-uint64_t MessID;
+unsigned long MessID;
 
-uint64_t msgPeriod = 5000; //Message check interval in ms
-uint32_t MsgSendNewPer;
+uint64_t msgPeriod = 10000; //Message check interval in ms (10 sec for testing)
+
 String S_msg;
 String BtnArraySend; // hold CSV of button array
 int value = 0;
@@ -116,7 +113,7 @@ void configModeCallback(WiFiManager *myWiFiManager) {
 void WiFiCP(uint8_t ResetAP)
 {
 	WiFiManager wifiManager;
-	wifiManager.setAPCallback(configModeCallback);
+	//wifiManager.setAPCallback(configModeCallback);
 	if (ResetAP){
 		wifiManager.resetSettings();
 		wifiManager.autoConnect("MoxieConfigAP");
@@ -141,7 +138,7 @@ void WiFiCP(uint8_t ResetAP)
 // send CSV line via MQTT
 void SendNewBtnMessage(){
 	MessID = millis();
-	BtnArraySend = "M" + MessID;
+	BtnArraySend = "M" + String(MessID);
 	for (size_t i = 0; i < NUM_BTNS; i++){
 		BtnArraySend = BtnArraySend + "," + String(BtnRecord[i]);
 	}
@@ -177,12 +174,6 @@ void setup() {
 	pinMode(LED_BUILTIN, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
 	Serial.begin(115200);
 	WiFiCP(true);
-	//WiFiManager wifiManager;
-	//wifiManager.setAPCallback(configModeCallback);
-	//wifiManager.autoConnect("AutoConnectAP");
-	//Serial.println("Print IP:");
-	//Serial.println(WiFi.localIP());
-   	MsgSendNewPer = 0;
 	for (size_t i = 0; i < 5; i++)
 		pinMode(OutPins[i], OUTPUT);
 	for (size_t i = 0; i < 4; i++)
@@ -208,24 +199,22 @@ void loop() {
             if (BtnState[i] == 0)
                BtnState[i] = millis(); // store curr mils of current button pressed 
 	   }
-   }
-
-   // this is where we record the button press count / reset after the lockout period
-   for(size_t i = 0; i < NUM_BTNS; i++){
-      if((BtnState[i] > 0) && (now > (BtnState[i] + BTN_DELAY))){
-         Serial.print("button ");  // test code
-         Serial.print(i + 1);
-         Serial.println(" Cycled !");
-		   BtnRecord[i]++;
-         BtnState[i] = 0;
+	   // this is where we record the button press count / reset after the lockout period
+   	for(size_t i = 0; i < NUM_BTNS; i++){
+         if((BtnState[i] > 0) && (now > (BtnState[i] + BTN_DELAY))){
+            Serial.print("button ");  // test code
+            Serial.print(i + 1);
+            Serial.println(" Cycled !");
+		      BtnRecord[i]++;
+            BtnState[i] = 0;
+         }
       }
    }
-
-   // check status here, still test code needs work !!!!
+   // MQTT send / rec and check for incomming
 	GotMail = MTQ.update();
 	if (GotMail == true){
 
-		//**test code to be removed ****************
+		//**test code will blink LED on incomming****
 		Serial.print("message is: ");
 		S_msg = MTQ.GetMsg();
 		Serial.println(S_msg);
@@ -233,18 +222,18 @@ void loop() {
 		// ******************************************
 		GotMail = false;
 	}
-	// check every n sec to see if last message rc'ved
-	// if so push out a new message every 2 min (n * 20)
-   // remove for testing 
+	// push out button press results every x Msec
+   
 	if (now - lastMsg > msgPeriod) {
 		lastMsg = now;
-        SendNewBtnMessage();
+      SendNewBtnMessage();
+      Serial.println(BtnArraySend);
 		//**test code to be removed ****************
 		++value;
 		S_msg = "string message # " + String(value);
 		Serial.print("Publish message (main): ");
 		Serial.println(S_msg);
-		statusCode = MTQ.publish(S_msg);
+      statusCode = MTQ.publish(S_msg);
 		// ******************************************
     }
 }
